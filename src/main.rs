5aacaf7419b0b4005b28af6cb0530c256dec4b54
shurizzle <shura1991@gmail.com>
2025-04-25 23:20:30 +0200

    refactor: remove `unwrap`s

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use efivar::efi::{Variable, VariableFlags};
 
 fn _main() -> Result<(), Box<dyn std::error::Error>> {
     let mut manager = efivar::system();
-    let active = manager
-        .read(&Variable::new("BootCurrent"))
-        .unwrap()
-        .0
-        .as_slice()
-        .read_u16::<LittleEndian>()
-        .unwrap();
-    manager.write(&Variable::new("BootNext"), VariableFlags::default(), &active.to_le_bytes())?;
+    let active = manager.read(&Variable::new("BootCurrent"))?.0;
+    active.as_slice().read_u16::<LittleEndian>()?;
+    manager.write(
+        &Variable::new("BootNext"),
+        VariableFlags::default(),
+        &active,
+    )?;
     system_shutdown::reboot()?;
     Ok(())
 }
